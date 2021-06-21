b1beeac537afcb9a340109e282832cc095117e3d
Grace Cho <grace@splitmango.com>
2021-06-21 11:08:22 -0700

    Scroll on Chai tea product project

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-// import LocomotiveScroll from 'locomotive-scroll';
-
+// *** LocomotiveScroll ***
 const scroller = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true,
 });
+
+// *** GSAP ScrollTrigger ***
