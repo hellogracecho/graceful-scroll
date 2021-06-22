184a2b8d32b4863ab662dd4150f27dfb004b2671
Grace Cho <grace@splitmango.com>
2021-06-22 11:37:34 -0700

    Slick slider setup with  elements transform css animation

--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
-// *** LocomotiveScroll ***
-const scroller = new LocomotiveScroll({
-  el: document.querySelector('[data-scroll-container]'),
-  smooth: true,
-});
+// *** Slick Slider ***
+let $slider = $('.js-slider');
 
-// *** GSAP ScrollTrigger ***
+if ($slider.length) {
+  $slider.slick({
+    arrows: true,
+    prevArrow: $slider
+      .siblings('.js-options-slider-nav')
+      .children('.js-options-slider-prev'),
+    nextArrow: $slider
+      .siblings('.js-options-slider-nav')
+      .children('.js-options-slider-next'),
+    fade: true,
+    speed: 900,
+    cssEase: 'linear',
+    autoplay: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    rows: 0,
+  });
+}
+
+// On slide change
+$slider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+  console.log(currentSlide);
+  $('.slick-slide').removeClass('is-active');
+  $('.slick-current').addClass('is-active');
+});
