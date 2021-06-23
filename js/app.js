40f2cbee4072c5a4dfdf22f59db2332e1692d740
Grace Cho <grace@splitmango.com>
2021-06-23 12:03:07 -0700

    Slick slider added extra nav buttons with product image using data attribute

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // *** Slick Slider ***
-let $slider = $('.js-slider');
+const $slider = $('.js-slider');
 
 if ($slider.length) {
   $slider.slick({
@@ -21,5 +21,48 @@ if ($slider.length) {
   });
 }
 
-// TODO: work on slick dots with item box image replacement
-// ref: https://addwebsolution.com/blog/custom-nextprev-thumb-slick-slider
+// TODO: Add item box images to trigger slick as well
+$('#item-nav__prev').on('click', function () {
+  $slider.slick('slickPrev');
+});
+$('#item-nav__next').on('click', function () {
+  $slider.slick('slickNext');
+});
+
+let curr = $('.js-slider__single.slick-current');
+/* check if there is previous sibling, otherwise get last "a" in previous "LI"*/
+let prev = curr.prev().length
+  ? curr.prev()
+  : curr.parent().find('.js-slider__single').last();
+let next = curr.next().length
+  ? curr.next()
+  : curr.parent().find('.js-slider__single').first();
+
+console.log('init prev: ', prev.find('.item__img').data('item-nav'));
+console.log('init next: ', next.find('.item__img').data('item-nav'));
+
+const newPrevHtml = prev.find('.item__img').data('item-nav');
+const newNextHtml = next.find('.item__img').data('item-nav');
+$('#item-nav__prev').html('<img src = "' + newPrevHtml + '" />');
+$('#item-nav__next').html('<img src = "' + newNextHtml + '" />');
+
+$slider.on('afterChange', function (event, slick, currentSlide, nextSlide) {
+  console.log('slick fired');
+
+  let curr = $('.js-slider__single.slick-current');
+  /* check if there is previous sibling, otherwise get last "a" in previous "LI"*/
+  let prev = curr.prev().length
+    ? curr.prev()
+    : curr.parent().find('.js-slider__single').last();
+  let next = curr.next().length
+    ? curr.next()
+    : curr.parent().find('.js-slider__single').first();
+
+  console.log('init prev: ', prev.find('.item__img').data('item-nav'));
+  console.log('init next: ', next.find('.item__img').data('item-nav'));
+
+  const newPrevHtml = prev.find('.item__img').data('item-nav');
+  const newNextHtml = next.find('.item__img').data('item-nav');
+  $('#item-nav__prev').html('<img src = "' + newPrevHtml + '" />');
+  $('#item-nav__next').html('<img src = "' + newNextHtml + '" />');
+});
